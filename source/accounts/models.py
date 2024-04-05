0ef04afd9997ab970a2188b149194f9c7d38a385
Almas Rakyshbaev <kasper10.0711@gmail.com>
2024-04-05 08:42:55 +0500

    добавил модели профиля related_name

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -5,13 +5,14 @@ from django.db import models
 
 
 def directory_path(instance, filename):
-    username = instance.author.username.lower()
+    username = instance.user.username.lower()
     return os.path.join(f'accounts/avatars/{username}', f'{username}_image_{filename}')
 
 
 class AccountProfile(models.Model):
     user = models.ForeignKey(
         to=get_user_model(),
+        related_name='profile',
         on_delete=models.CASCADE,
         verbose_name="Пользователь"
     )
